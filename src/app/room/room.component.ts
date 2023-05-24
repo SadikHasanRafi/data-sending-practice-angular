39c7b8e0cc4ef5863fda60904cd8f1aa2d755c92
SadikHasanRafi <sadikkhasan@gmail.com>
2023-05-24 11:49:23 +0600

    parent component to child component data passing works fine

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./room.component.css']
 })
 
-@Input() room:string=""
-export class RoomComponent {
 
+export class RoomComponent {
+ @Input() room:any;
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -10,25 +10,26 @@ interface Room {
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
+  // template: `
+  // <p>rooms works!</p>
+  // <app-room [room]="roomList" ></app-room>
+  // `,
   styleUrls: ['./rooms.component.css']
 })
-
 
 export class RoomsComponent implements OnInit {
 
   roomList:Room[] = [];
 
-  
-  normalData:string="sjdbsm"
-
   ngOnInit(): void {
-
     this.roomList = [
       { id: 1, name: "Conference Room A", capacity: 10, location: "Floor 1" },
       { id: 2, name: "Conference Room B", capacity: 8, location: "Floor 2" },
       { id: 3, name: "Meeting Room X", capacity: 6, location: "Floor 3" },
       // Add more rooms as needed
     ];
+
+    const rooms = this.roomList
 
     
 
